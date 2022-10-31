9c0ad9ee5fdb4d09e6bd675cac76d4546192ea69
Scott Davis <scott@goodjob.io>
2022-10-31 17:02:19 -0600

    create basic component to query data from api

---
+++ src/components/Characters.js
@@ -0,0 +1,26 @@
+import { useQuery } from 'react-query';
+
+export default function Characters() {
+  const fetchCharacters = async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character")
+    return response.json();
+  }
+  
+  const {data, status} = useQuery("characters", fetchCharacters)
+
+  if(status === "loading"){
+    return <div>Loading...</div>;
+  }
+
+  if(status === "error"){
+    return <div>Error</div>;
+  }
+
+  return (
+    <div>
+     {data.results.map((character) => (
+      <div>{character.name}</div>
+     ))}
+    </div>
+  )
+}
