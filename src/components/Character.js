8a9ae88528c3b327e66d2db9df63f8c4df32fa52
Scott Davis <scott@goodjob.io>
2022-11-01 10:41:42 -0600

    add alt tag to character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,16 @@
 export default function Character({ character }) {
   return (
-    <div className="card">
-      <img src={character.image} alt="" />
-      <div className="text-container">
+    <div className='card'>
+      <img
+        src={character.image}
+        alt={character.name}
+      />
+      <div className='text-container'>
         <h3>{character.name}</h3>
-        <p className="status">
+        <p className='status'>
           {character.status} - {character.species}
         </p>
-        <p className="title">Last seen on</p>
+        <p className='title'>Last seen on</p>
         <p>{character.location.name}</p>
       </div>
     </div>
